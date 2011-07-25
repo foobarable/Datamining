22a1eba6a4680878597d5e73695d84abf2b52edf
Ralph Krimmel <thirsty@milk-and-cookies.net>
2011-07-25 16:08:54 +0200

    aufgabe 2

--- blatt1/aufgabe2.m
+++ blatt1/aufgabe2.m
@@ -1,8 +1,45 @@
-#Aufgabe2:Zufallsdaten
+#Aufgabe2:Zufallsdatene
 
 nDims = 2;
-nVecs = 100;
+nDataSets = 10;
+%NVecsVec = [100,1000,10000];
+NVecsVec = [100];
 
-XdataMat = rand(nDims,nVecs);
 
-plot(XdataMat(1,:),XdataMat(2,:),'x')
+for j = 1:length(NVecsVec)
+
+
+	for i = 1:nDataSets
+
+		XDataMat = rand(nDims,NVecsVec(j));
+		meanVec = mean(XDataMat,2);
+		XDataMat = XDataMat - repmat(meanVec,1,NVecsVec(j));
+		AngleVarianceMat = [[1:360];zeros(1,360)];
+
+		maxVar = 0;
+		maxAngle = 0;
+
+		for alphaAngle = 1:360;
+
+			RotMat = [ cos(alphaAngle) sin(alphaAngle); -sin(alphaAngle) cos(alphaAngle) ];
+			RotDataMat = RotMat*XDataMat;
+			xVar = var(RotDataMat(:,1));
+			AngleVarianceMat(2,alphaAngle) = xVar;	
+			if xVar > maxVar
+				maxVar = xVar;
+				maxAngle = alphaAngle;
+			end
+
+		end
+		plot(AngleVarianceMat(1,:),AngleVarianceMat(2,:))
+		pause(1)
+		
+		maxAngle
+		maxVar
+		AngleVarianceMat;
+	%plot(XDataMat(1,:),XDataMat(2,:),'x')
+	end
+
+end
+
+
