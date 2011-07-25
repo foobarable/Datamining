4c5b797a2d4c3dee88786748ea9fee6eab5578c6
Christian Mueller <chrismul@bi-89.gobics.de>
2011-07-25 11:41:35 +0200

    first files

---
+++ blatt1/aufgabe1.m
@@ -0,0 +1,5 @@
+#Aufgabe1: Eigenwerte und Eigenvektoren
+
+AMat = [ 3, -2; -4, 1 ];
+det(AMat)
+[EigenVecMat, EigenValMat] = eig(AMat)
---
+++ blatt1/aufgabe2.m
@@ -0,0 +1,8 @@
+#Aufgabe2:Zufallsdaten
+
+nDims = 2;
+nVecs = 100;
+
+XdataMat = rand(nDims,nVecs);
+
+plot(XdataMat(1,:),XdataMat(2,:),'x')
